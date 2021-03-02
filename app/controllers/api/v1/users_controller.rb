21949c3baa082b34c43a4dfa8e8bd2dccdda0a66
Marcelo de Alencar <dealencarmarcelo@gmail.com>
2021-03-02 00:16:01 -0300

    add User create request - request tests successful

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -10,9 +10,24 @@ class Api::V1::UsersController < ApplicationController
         render json: @user
     end
 
+    def create
+        @user = User.new(create_user_params)
+
+        if @user.save
+            render json: @user, status: :created
+        else
+            render json: @user.errors, status: :unprocessable_entity
+        end
+    end
+
     private
 
     def set_user
         @user = User.find(params[:id])
+    end
+
+    def create_user_params
+        params.require(:user).permit(:fullname, :surname, :email,
+                                    :password, :password_confirmation)
     end
 end
--- spec/requests/api/v1/user_request_spec.rb
+++ spec/requests/api/v1/user_request_spec.rb
@@ -3,6 +3,19 @@ require 'shoulda/matchers'
 
 describe 'User', type: :request do
 
+    let(:params) do
+        {
+            user: {
+                fullname: Faker::Name.name_with_middle,
+                surname: Faker::Name.first_name,
+                email: Faker::Internet.email,
+                phone: '+5581999999999',
+                password: '123456',
+                password_confirmation: '123456'
+            }
+        }
+    end
+
     let(:user) { create(:user) }
     
     describe 'get /users' do
@@ -29,6 +42,45 @@ describe 'User', type: :request do
         context 'when user does not exist' do
             it 'raises RecordNotFound when not found' do
                 expect { get api_v1_user_path(0) }.to raise_error(ActiveRecord::RecordNotFound)
+            end
+        end
+    end
+
+    describe 'post /users' do
+        let(:created_user) { User.last }
+
+        context 'when fullname, surname and email is valid' do
+            let(:json) { JSON.parse(response.body, symbolize_names: true) }
+
+            before { post api_v1_users_path, params: params }
+
+            it { expect(response).to have_http_status(:created) }
+            it { expect(created_user.fullname).to eq(json.dig(:fullname)) }
+        end
+
+        context 'when fullname, surname and email is invalid' do
+            it 'returns unprocessable_entity with invalid fullname' do
+                post api_v1_users_path, params: { user: { fullname: nil } }
+                expect(response).to have_http_status(:unprocessable_entity)
+            end
+
+            it 'returns unprocessable_entity with invalid surname' do
+                post api_v1_users_path, params: { user: { surname: nil } }
+                expect(response).to have_http_status(:unprocessable_entity)
+            end
+
+            it 'returns 422 with invalid email' do
+                post api_v1_users_path, params: { user: { email: 'email_without_format.com' } }
+                expect(response).to have_http_status(:unprocessable_entity)
+            end
+
+            it 'returns unprocessable_entity with nil email' do
+                post api_v1_users_path, params: { user: { email: nil } }
+                expect(response).to have_http_status(:unprocessable_entity)
+            end
+
+            it 'do not create an user' do
+                expect { created_user.name }.to raise_error(NoMethodError)
             end
         end
     end
