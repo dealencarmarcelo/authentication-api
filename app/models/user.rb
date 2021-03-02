8cd935675b146808183d0dd3f94f4720612fc1ba
Marcelo de Alencar <dealencarmarcelo@gmail.com>
2021-03-01 22:17:50 -0300

    add password length validation - model test successful

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
     has_secure_password
 
-    validates_presence_of :fullname, :surname, :email, :password_digest
+    validates_presence_of :fullname, :surname, :email
+    validates :password_digest, presence: true, length: { minimum: 6 }
 
     validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
     validates_uniqueness_of :email, case_sensitive: false
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 require 'shoulda/matchers'
 
 describe User, type: :model do
-    
+
     it { should validate_presence_of(:fullname) }
     it { should validate_presence_of(:surname) }
 
@@ -12,5 +12,6 @@ describe User, type: :model do
     it { should validate_uniqueness_of(:email).case_insensitive }
 
     it { should validate_presence_of(:password_digest) }
-
+    it { should validate_length_of(:password_digest).is_at_least(6) }
+    
 end
